f3a418799cc4d096d495f493a2f79044a1f5cf18
bhavika-89 <bhavikathakkar089@gmail.com>
2026-02-07 15:23:42 +0530

    commit

--- main1.js
+++ main1.js
@@ -23,5 +23,26 @@ onload = () => {
   }
 });
 
+const heartsContainer = document.querySelector(".hearts");
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "💗";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = 4 + Math.random() * 4 + "s";
+  heart.style.fontSize = 16 + Math.random() * 24 + "px";
+
+  heartsContainer.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 8000);
+}
+
+setInterval(createHeart, 500);
+
+
   };
   
